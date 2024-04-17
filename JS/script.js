7e1d4e9a89093cd66bbd2649848272f4b6869a52
ALouKatraz <louise.casteleyn@gmail.com>
2024-04-17 20:27:51 +0200

    Correction et modification de l'apparence et des textes

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,22 @@
 const reviews = [
-    {
-        image: "https://picsum.photos/400",
-        name1: "Louise",
-        job: "Codeuse",
-        info: "Louise, codeuse en formation. Elle apprend chaque jour et ne lâche pas car elle a enfin trouvé sa voie!.",
-    },
-    {
-        image: "https://picsum.photos/300",
-        name1: "Leïa",
-        job: "Informaticienne",
-        info: "Jean, Informaticien depuis <<le début de l'internet>> comme il aime à le dire.",
-    },
-    {
-        image: "https://picsum.photos/200",
-        name1: "Lydia",
-        job: "cheffe RH",
-        info: "Lydia, j'aime placer les gens au bon endroit.",
-    },
+  {
+    image: "https://picsum.photos/400",
+    name1: "Louise",
+    job: "Codeuse",
+    info: "Louise, codeuse en formation. Elle apprend chaque jour et ne lâche pas car elle a enfin trouvé sa voie!.",
+  },
+  {
+    image: "https://picsum.photos/300",
+    name1: "Leïa",
+    job: "Informaticienne",
+    info: "Leïa, Informaticien depuis le début de l'internet comme il aime à le dire.",
+  },
+  {
+    image: "https://picsum.photos/200",
+    name1: "Lydia",
+    job: "cheffe RH",
+    info: "Lydia, j'aime placer les gens au bon endroit.",
+  },
 ];
 const nbr = reviews.length;
 const image = document.getElementById("image");
@@ -24,19 +24,19 @@ const name1 = document.getElementById("name1");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
-let currentReview = 0
+let currentReview = 0;
 
 function displayReview(currentReview) {
-    console.log("currentReview", currentReview);
-    image.src = currentReview.image;
-    name1.innerHTML = currentReview.name1;
-    job.innerHTML = currentReview.job;
-    info.innerHTML = currentReview.info;
-    }
+  console.log("currentReview", currentReview);
+  image.src = currentReview.image;
+  name1.innerHTML = currentReview.name1;
+  job.innerHTML = currentReview.job;
+  info.innerHTML = currentReview.info;
+}
 
-function test(){
-    const randomNumber = Math.floor(Math.random()* nbr);
-    displayReview(reviews[randomNumber]);
-    }
+function test() {
+  const randomNumber = Math.floor(Math.random() * nbr);
+  displayReview(reviews[randomNumber]);
+}
 const Surprise = document.getElementById("button");
 Surprise.addEventListener("click", test);
